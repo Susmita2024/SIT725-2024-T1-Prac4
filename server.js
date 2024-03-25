8e9f265c5121ee68ffa1ba5f856b4a59d3e40a42
Susmita <susmita.supu@gmail.com>
2024-03-25 16:52:02 +1100

    updated script section in package.json

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express") 
+var app = express()
+var port = process.env.port || 3000;
+
+
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello SUPU!')
+  })
